4a0eac8879885a0875e50d6d0f9c8068d20c38cb
Ishan Sharma <ishandeveloper@outlook.com>
2020-06-10 23:47:14 +0530

    App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+import { formatDate } from "./helpers";
+import { API_URL } from "./api";
+
+// Components
+import WeatherData from "./components/WeatherData";
+import Info from "./components/Info";
+import Unit from "./components/Unit";
+import Previous from "./components/Previous";
+
+import Image from "./img/mars.jpg";
+
+import {
+  AppWrapper,
+  GlobalStyle,
+  MarsWeather,
+  InfoWrapper,
+} from "./App.styles";
+
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [weather, setWeather] = useState([]);
+  const [selectSol, setSelectSol] = useState(true);
+  const [previous, setPrevious] = useState(false);
+  const [metric, setMetric] = useState(true);
+
+  console.log(weather);
+
+  useEffect(() => {
+    const fetchFromAPI = async () => {
+      const weather = await fetch(API_URL).then((data) => data.json());
+      console.log(weather);
+      const marsWeather = weather.sol_keys.map((solKey) => {
+        return {
+          sol: solKey,
+          maxTemp: weather[solKey].AT?.mx || "No Data",
+          minTemp: weather[solKey].AT?.mn || "No Data",
+          windSpeed: Math.round(weather[solKey].HWS?.av || 0),
+          windDirectionDegrees:
+            weather[solKey].WD?.most_common?.compass_degrees || 0,
+          date: formatDate(new Date(weather[solKey].First_UTC)),
+        };
+      });
+      setWeather(marsWeather);
+      setSelectSol(marsWeather.length - 1);
+      setLoading(false);
+    };
+
+    fetchFromAPI();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle bgImage={Image} />
+      <AppWrapper>
+        <MarsWeather>
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <>
+              <h1 className="main-title">
+                Latest Weather at Elysium Plantitia
+              </h1>
+              <WeatherData sol={weather[selectSol]} isMetric={metric} />
+              <InfoWrapper>
+                <Info />
+                <Unit metric={metric} setMetric={setMetric} />
+              </InfoWrapper>
+            </>
+          )}
+        </MarsWeather>
+        <Previous
+          weather={weather}
+          previous={previous}
+          setPrevious={setPrevious}
+          setSelectedSol={setSelectSol}
+          isMetric={metric}
+        />
+      </AppWrapper>
+    </>
   );
-}
+};
 
 export default App;
