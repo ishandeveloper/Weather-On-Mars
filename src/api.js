005f6acb960ab714bd0c67b4226af60a10a88581
Ishan Sharma <ishandeveloper@outlook.com>
2020-06-10 23:47:06 +0530

    App

---
+++ src/api.js
@@ -0,0 +1,4 @@
+require("dotenv").config();
+// const API_KEY = process.env.API_KEY;
+const API_KEY="DEMO_KEY";
+export const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`;
